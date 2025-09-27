11cbdaa6ed2892df530c03139202fb87e3648ccb
NoXi <nf@noxitech.dk>
2025-09-27 18:45:47 +0200

    Bump version to 1.2-SNAPSHOT in pom.xml and add CacheFlag import in Main.java

--- src/main/java/dk/noxitech/serverlogs/Main.java
+++ src/main/java/dk/noxitech/serverlogs/Main.java
@@ -5,6 +5,7 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
